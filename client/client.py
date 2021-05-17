d9bd8277d0122c7c2bb2311039e202a1aad4759d
Fabel <daprostovseeto@gmail.com>
2021-05-17 20:15:04 +0300

    add config

--- client/client.py
+++ client/client.py
@@ -4,10 +4,9 @@ from api import divider_pb2
 from api import divider_pb2_grpc
 
 
-def run(x: int, y: int, channel: grpc.Channel = None):
-
+def run(target: str, x: int, y: int, channel: grpc.Channel = None):
     if not channel:
-        channel = grpc.insecure_channel('localhost:8191')
+        channel = grpc.insecure_channel(target=target)
 
     with channel as c:
         client = divider_pb2_grpc.DividerStub(channel=c)
--- manage.py
+++ manage.py
@@ -1,5 +1,10 @@
-from client.client import run
 import sys
 
+from client.client import run
+from config import config
+
 if __name__ == '__main__':
-    run(x=int(sys.argv[1]), y=int(sys.argv[2]))
+    config = config()
+    host = config.get('client', 'host')
+    port = config.get('client', 'port')
+    run(target=f'{host}:{port}', x=int(sys.argv[1]), y=int(sys.argv[2]))
